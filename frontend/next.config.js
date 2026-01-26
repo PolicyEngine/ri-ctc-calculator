f06c96c8b7a15913a7650d8724b9fea3ede6938f
David Trimmer <david@policyengine.org>
2026-01-26 15:07:09 -0500

    config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'standalone',
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8081',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
   },
 }
 
